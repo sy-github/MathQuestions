93a301be71cbb1adc5e0458b22795294e45ce04d
Ying Shi <shi_ying@outlook.com>
2017-10-12 09:43:26 +0100

    Add project files.

---
+++ FrontEnd/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ShiYing.MathQuestions.FrontEnd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private App _app = (App)App.Current;
+
+        private TimeSpan _countdownAdd;
+        private DispatcherTimer _timerAdd = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, (object o, EventArgs e) =>
+        {
+        }, App.Current.Dispatcher);
+
+        public MainWindow() => InitializeComponent();
+
+        #region Event Handlers
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            SetTextBoxContent(AddTextBox, _app.Generator.GetAdditionQuestions());
+
+            _timerAdd.Tag = TimeSpan.FromMinutes(2);
+            CountdownText.Text = ((TimeSpan)_timerAdd.Tag).ToString(@"mm\:ss");
+            _timerAdd.Start();
+        }
+
+        private void ButtonSub_Click(object sender, RoutedEventArgs e)
+        {
+            SetTextBoxContent(SubTextBox, _app.Generator.GetSubtractionQuestions());
+        }
+
+        #endregion
+
+        private void SetTextBoxContent(TextBox t, IEnumerable<string> lines)
+        {
+            t.Text = string.Empty;
+            foreach (var line in lines)
+                t.Text += line + '\n';
+            t.ScrollToHome();
+        }
+    }
+}
---
+++ Generator/Generator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShiYing.MathQuestions
+{
+    public class GeneratorConfig
+    {
+        public bool Add_WithinTen = false;
+    }
+
+    public class Generator
+    {
+        private Random _rand = new Random();
+
+        public GeneratorConfig Config { get; set; }
+
+        public IEnumerable<string> GetAdditionQuestions()
+        {
+            int maxInt = 100;
+            const int count = 50;
+
+            var list = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var a = _rand.Next(maxInt);
+                var b = _rand.Next(maxInt);
+
+                list.Add(string.Format("{0} + {1} = ", a, b));
+            }
+
+            return list.Distinct();
+        }
+
+        public IEnumerable<string> GetSubtractionQuestions()
+        {
+            int maxInt = 100;
+            const int count = 50;
+
+            var list = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var a = _rand.Next(maxInt);
+                var b = _rand.Next(maxInt);
+
+                if (a > b)
+                    list.Add(string.Format("{0} - {1} = ", a, b));
+                else
+                    list.Add(string.Format("{0} - {1} = ", b, a));
+            }
+
+            return list.Distinct();
+        }
+    }
+}
